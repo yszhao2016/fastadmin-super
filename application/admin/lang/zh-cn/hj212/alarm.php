1e66a9503ccbee619a26707942cb60cfa98dcfe7
tang.qianqian <tang.qianqian@utooo.com>
2022-02-14 17:49:10 +0800

    报警设置

---
+++ application/admin/lang/zh-cn/hj212/alarm.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'Codeid'     => '检测因子',
+    'Alarm_min'  => '报警最小值',
+    'Alarm_max'  => '报警最大值',
+    'Warn_mix'   => '预警最小值',
+    'Warn_max'   => '预警最大值',
+    'Created_at' => '创建时间',
+    'Updated_at' => '更新时间'
+];
---
+++ public/assets/js/backend/hj212/alarm.js
@@ -0,0 +1,55 @@
+define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefined, Backend, Table, Form) {
+
+    var Controller = {
+        index: function () {
+            // 初始化表格参数配置
+            Table.api.init({
+                extend: {
+                    index_url: 'hj212/alarm/index' + location.search,
+                    add_url: 'hj212/alarm/add',
+                    edit_url: 'hj212/alarm/edit',
+                    del_url: 'hj212/alarm/del',
+                    multi_url: 'hj212/alarm/multi',
+                    import_url: 'hj212/alarm/import',
+                    table: 'hj212_alarm',
+                }
+            });
+
+            var table = $("#table");
+
+            // 初始化表格
+            table.bootstrapTable({
+                url: $.fn.bootstrapTable.defaults.extend.index_url,
+                pk: 'id',
+                sortName: 'id',
+                columns: [
+                    [
+                        {checkbox: true},
+                        {field: 'id', title: __('Id')},
+                        {field: 'codeId', title: __('Codeid')},
+                        {field: 'alarm_min', title: __('Alarm_min'), operate:'BETWEEN'},
+                        {field: 'alarm_max', title: __('Alarm_max'), operate:'BETWEEN'},
+                        {field: 'warn_mix', title: __('Warn_mix'), operate:'BETWEEN'},
+                        {field: 'warn_max', title: __('Warn_max'), operate:'BETWEEN'},
+                        {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
+                    ]
+                ]
+            });
+
+            // 为表格绑定事件
+            Table.api.bindevent(table);
+        },
+        add: function () {
+            Controller.api.bindevent();
+        },
+        edit: function () {
+            Controller.api.bindevent();
+        },
+        api: {
+            bindevent: function () {
+                Form.api.bindevent($("form[role=form]"));
+            }
+        }
+    };
+    return Controller;
+});
