dab53a44436b5faa4438010cca3a908f79eb0705
zhaoys <18761152733@163.com>
2022-02-15 17:17:11 +0800

    feature add 站点地图

---
+++ public/assets/js/backend/hj212/pointmap.js
@@ -0,0 +1,21 @@
+define(['gdmap'], function ($, undefined, Backend, Table, Form) {
+
+    var Controller = {
+        index: function () {
+
+
+        },
+        add: function () {
+            Controller.api.bindevent();
+        },
+        edit: function () {
+            Controller.api.bindevent();
+        },
+        api: {
+            bindevent: function () {
+                Form.api.bindevent($("form[role=form]"));
+            }
+        }
+    };
+    return Controller;
+});
