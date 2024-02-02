06fb25b052a0ae16384ccb5e6a79042b9677f1a7
zhao.=youshui <zhao.youshui@utooo.com>
2024-02-02 13:41:41 +0800

    fix bug alarm data page

--- application/admin/controller/hj212/AlarmData.php
+++ application/admin/controller/hj212/AlarmData.php
@@ -20,4 +20,30 @@ class AlarmData extends Backend
     }
 
 
+    public function index()
+    {
+        $this->request->filter(['strip_tags', 'trim']);
+        if ($this->request->isAjax()) {
+            //如果发送的来源是Selectpage，则转发到Selectpage
+            if ($this->request->request('keyField')) {
+                return $this->selectpage();
+            }
+            list($where, $sort, $order, $offset, $limit) = $this->buildparams();
+
+            $list = $this->model
+                ->alias('a')
+                ->field('a.id,a.data_id,qn,cn,mn,cp_datatime,site_name,a.created_at as created_at')
+                ->join('hj212_device d', 'a.mn=d.device_code', 'left')
+                ->join('hj212_site s', 'd.site_id=s.id', 'left')
+                ->where($where)
+                ->order($sort, $order)
+                ->paginate($limit);
+
+            $result = array("total" => $list->total(), "rows" => $list->items());
+
+            return json($result);
+        }
+        return $this->view->fetch();
+    }
+
 }
